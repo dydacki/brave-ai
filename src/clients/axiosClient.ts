f6a65156feef280c3b67e421b775a05ba8c6489a
Wojciech Dydacki <wdydacki@volvocars.com>
2024-03-24 17:59:38 +0100

    refactor: add prettier options

--- src/clients/axiosClient.ts
+++ src/clients/axiosClient.ts
@@ -1,21 +1,21 @@
 import axios, {AxiosInstance} from 'axios';
-import { TaskDevClientConfig } from '../model/model';
+import {TaskDevClientConfig} from '../model/model';
 
-export const useAxcios = (config: TaskDevClientConfig): AxiosInstance => {
-    const client = axios.create({
-        baseURL: `${config.baseUrl}/`,
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        timeout: 10000,
+export const useAxios = (config: TaskDevClientConfig): AxiosInstance => {
+  const client = axios.create({
+    baseURL: `${config.baseUrl}/`,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    timeout: 10000,
+  });
+
+  if (config.debug) {
+    client.interceptors.request.use(request => {
+      console.log('Sending request:', request);
+      return request;
     });
+  }
 
-    if (config.debug) {
-        client.interceptors.request.use((request) => {
-            console.log('Sending request:', request);
-            return request;
-        });
-    }
-
-    return client;
-}
+  return client;
+};
--- src/clients/taskDevClient.ts
+++ src/clients/taskDevClient.ts
@@ -1,65 +1,64 @@
-import {useAxcios} from "./axiosClient";
-import { AxiosInstance } from "axios";
-import {Answer, TaskDevClientConfig, TokenResponse, AnswerResponse, Task} from "../model/model";
+import {useAxios} from './axiosClient';
+import {AxiosInstance} from 'axios';
+import {Answer, TaskDevClientConfig, TokenResponse, AnswerResponse, Task} from '../model/model';
 
 export class TaskDevClient {
-    private axios: AxiosInstance;
-    private readonly apiKey: string;
+  private axios: AxiosInstance;
+  private readonly apiKey: string;
 
-    constructor(config: TaskDevClientConfig) {
-        this.axios = useAxcios(config);
-        this.apiKey = config.apiKey;
-    }
+  constructor(config: TaskDevClientConfig) {
+    this.axios = useAxios(config);
+    this.apiKey = config.apiKey;
+  }
 
-    private getAuthorizationBody() {
-        return {
-            apiKey: this.apiKey,
-        };
-    }
+  private getAuthorizationBody() {
+    return {
+      apiKey: this.apiKey,
+    };
+  }
 
-    private printErrorDetails (error: any): void {
-        if (error.response) {
-            console.log(error.response.data);
-            console.log(error.response.status);
-            console.log(error.response.headers);
-        } else if (error.request) {
-            console.log(error.request);
-        } else {
-            console.log('Error', error.message);
-        }
-        console.log(error.config);
+  private printErrorDetails(error: any): void {
+    if (error.response) {
+      console.log(error.response.data);
+      console.log(error.response.status);
+      console.log(error.response.headers);
+    } else if (error.request) {
+      console.log(error.request);
+    } else {
+      console.log('Error', error.message);
     }
+    console.log(error.config);
+  }
 
-    async getToken(taskName: string): Promise<TokenResponse> {
-        try {
-            const response = await this.axios.post<TokenResponse>(`/token/${taskName}`, this.getAuthorizationBody());
-            return response.data;
-        } catch (error) {
-            this.printErrorDetails(error);
-            throw error;
-        }
+  async getToken(taskName: string): Promise<TokenResponse> {
+    try {
+      const response = await this.axios.post<TokenResponse>(`/token/${taskName}`, this.getAuthorizationBody());
+      return response.data;
+    } catch (error) {
+      this.printErrorDetails(error);
+      throw error;
     }
+  }
 
-    async submitAnswer(answer: Answer, token: string): Promise<AnswerResponse> {
-        try {
-            const response = await this.axios.post<AnswerResponse>(`/answer/${token}`,
-            {
-                answer: answer.answer,
-            });
-            return response.data;
-        } catch (error) {
-            this.printErrorDetails(error);
-            throw error;
-        }
+  async submitAnswer(answer: Answer, token: string): Promise<AnswerResponse> {
+    try {
+      const response = await this.axios.post<AnswerResponse>(`/answer/${token}`, {
+        answer: answer.answer,
+      });
+      return response.data;
+    } catch (error) {
+      this.printErrorDetails(error);
+      throw error;
     }
+  }
 
-    async getTask(token: string): Promise<Task> {
-        try {
-            const response = await this.axios.get<Task>(`/task/${token}`);
-            return response.data;
-        } catch (error) {
-            this.printErrorDetails(error);
-            throw error;
-        }
+  async getTask(token: string): Promise<Task> {
+    try {
+      const response = await this.axios.get<Task>(`/task/${token}`);
+      return response.data;
+    } catch (error) {
+      this.printErrorDetails(error);
+      throw error;
     }
+  }
 }
